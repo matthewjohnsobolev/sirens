fcbeb40af01d615ab1e136979d5655b944678977
root <root@localhost.localdomain>
2026-09-09 08:15:49 +0000

    feat(map): прибрати посилання на збій з атрибуції мапи

--- config.py
+++ config.py
@@ -111,9 +111,7 @@ MAP_TILES_ATTRIBUTION = os.getenv(
     "MAP_TILES_ATTRIBUTION",
     '&copy;&nbsp;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     '<span class="map-attribution-divider" aria-hidden="true">&middot;</span>'
-    '&copy;&nbsp;<a href="https://carto.com/attributions">CARTO</a>'
-    '<span class="map-attribution-divider" aria-hidden="true">&middot;</span>'
-    '<a href="/issue">Збій?</a>',
+    '&copy;&nbsp;<a href="https://carto.com/attributions">CARTO</a>',
 ).strip()
 
 SENTRY_DSN = os.getenv("SENTRY_DSN", "")
